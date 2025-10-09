7e1513e4ecf34e31ced45f1ff75966187e25edb1
EthanForster <ethanjforster@hotmail.co.uk>
2025-10-10 00:22:22 +0100

    TestPush
    added the start of a basic enemy pathfinding script. also shoved ghost guys into the sprites folder

---
+++ Spirits Harvest/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float speed;
+
+    Transform targetPoint;
+    int waypointIndex;
+
+    void Start()
+    {
+        targetPoint = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = targetPoint.position -transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, targetPoint.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        waypointIndex = waypointIndex + 1;
+        targetPoint = Waypoints.points[waypointIndex];
+    }
+}
