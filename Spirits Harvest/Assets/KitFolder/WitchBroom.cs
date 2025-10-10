037bb8ea1ecf369302a0f2397885ce757279d293
CreepKit <23651079@stu.mmu.ac.uk>
2025-10-10 13:18:37 +0100

    Commenting

--- Spirits Harvest/Assets/KitFolder/WitchBroom.cs
+++ Spirits Harvest/Assets/KitFolder/WitchBroom.cs
@@ -10,6 +10,7 @@ public class WitchBroom : MonoBehaviour
     }
 
     // What is needed - Use the triangle as the broom and then have it so that when it touches something, it is able to knock it back and do 1hp.
+    // Broom will need a collider, so will the enemy. When they touch, enemy knocked back. 
 
     public void BroomAttack()
     {
