222dcccda78492f04928f4357112cc61679f011c
Kit <23651079@stu.mmu.ac.uk>
2025-10-15 13:08:50 +0100

    Commenting a To-Do List

--- Spirits Harvest/Assets/KitFolder/JackOLantern.cs
+++ Spirits Harvest/Assets/KitFolder/JackOLantern.cs
@@ -5,7 +5,8 @@ public class JackOLantern : MonoBehaviour
     //The Jack O Lantern will take damage when an enemy comes into contact with it
     //It also attacks if an enemy comes in range of it 
     //The attack is a flamethrower, that shoots out in front of it (depending on where it is placed).
-    //The attack will need a cooldown or a way to detect if enemies come nearby. Similar to the peashooter in pvz
+    //Will need to have code to detect the nearest waypoint and if the enemy goes onto it 
+    //The attack will need a cooldown. Similar to the peashooter in pvz
     //
 
 {
