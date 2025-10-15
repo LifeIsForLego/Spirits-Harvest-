638c7cf42df1f474055d348417032a561d5dd766
Kit <23651079@stu.mmu.ac.uk>
2025-10-15 12:43:27 +0100

    Made a basic method for taking damage

--- Spirits Harvest/Assets/KitFolder/JackOLantern.cs
+++ Spirits Harvest/Assets/KitFolder/JackOLantern.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 
 public class JackOLantern : MonoBehaviour
+
+    //The Jack O Lantern will take damage when an enemy comes into contact with it
+    //It also attacks if an enemy comes in range of it 
+    //The attack is a flamethrower, that shoots out in front of it (depending on where it is placed).
+    //The attack will need a cooldown or a way to detect if enemies come nearby. Similar to the peashooter in pvz
+    //
+
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] public float lanternHealth = 10.0f;
+    [SerializeField] public float lanternDamage = 10.0f;
+   
+    
     void Start()
     {
         
@@ -13,4 +25,10 @@ public class JackOLantern : MonoBehaviour
     {
         
     }
+
+    public void lanternTakeDamage(float damage)
+    {
+        lanternDamage -= damage;
+    }
+
 }
